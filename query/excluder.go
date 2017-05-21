292d9ed95fcdfc1b126655c7f0fb1818e82b4fa5
Brian Jones <jonesbrianc26@gmail.com>
2017-05-21 18:36:29 -0500

    Query excluder by regexp fix build regexp

--- query/excluder.go
+++ query/excluder.go
@@ -1,8 +1,8 @@
 package query
 
 import (
-	"fmt"
 	"regexp"
+	"strings"
 )
 
 type Excluder interface {
@@ -16,7 +16,7 @@ type RegexpExclude struct {
 }
 
 //ShouldExclude will return a boolean denoting whether or not the path should be excluded based on the user input
-func (r *RegexpExclude) ShouldExclude(path string) bool {
+func (r RegexpExclude) ShouldExclude(path string) bool {
 	regex := r.buildRegex()
 	if b, ok := regexp.MatchString(regex, path); ok == nil {
 		return b
@@ -24,26 +24,56 @@ func (r *RegexpExclude) ShouldExclude(path string) bool {
 	return false
 }
 
-func (r *RegexpExclude) buildRegex() string {
+func (r RegexpExclude) buildRegex() string {
+	var regex string
+	var prev string
+	var curr string
 	for _, exclusion := range r.exclusions {
-		fmt.Println(exclusion)
+		prev = curr
+		if strings.HasSuffix(exclusion, "/") {
+			curr =
+				or(mustEnd(mustBegin(escape(exclusion))),
+					mustEnd(mustBegin(escape(exclusion[:len(exclusion)-1]))))
+		} else {
+			curr =
+				or(mustEnd(mustBegin(escape(exclusion))),
+					mustEnd(mustBegin(escape(exclusion+"/"))))
+		}
+		regex = or(prev, curr)
 	}
-	return ""
+	return regex
 }
 
 func or(p1, p2 string) string {
+	if p1 == "" {
+		return p2
+	}
+	if p2 == "" {
+		return p1
+	}
 	return p1 + "|" + p2
 }
 
-func matchEnd(p string) string {
+func mustEnd(p string) string {
 	return p + "$"
 }
 
-func matchBegin(p string) string {
+func mustBegin(p string) string {
 	return "^" + p
 }
 
 func group(expression string) string {
 	return "(" + expression + ")"
+}
 
+func escape(expression string) string {
+	var str string
+	for _, r := range expression {
+		if r == '.' {
+			str += "\\"
+		}
+		str += string(r)
+	}
+
+	return str
 }
