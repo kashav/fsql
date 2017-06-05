65350f6808d271094af9a0f2f43c284f961a2b4d
Kashav Madan <kshvmdn@gmail.com>
2017-06-05 16:57:54 -0400

    Dynamically retrieve file mod. time & size.
    Since Travis clones a fresh repo every build, we can't rely on local
    modification times in tests (see the comment in bb83481 for more details).
    Now, we manually walk ./testdata to retrieve time and size values.

--- fsql_test.go
+++ fsql_test.go
@@ -5,10 +5,55 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 	"reflect"
 	"strings"
 	"testing"
+	"time"
 )
+
+var files = map[string]*os.FileInfo{}
+
+func getAttr(path string, attr string) string {
+	// If the files map is empty, walk ./testdata and populate it!
+	if len(files) == 0 {
+		filepath.Walk("./testdata", func(path string, info os.FileInfo, err error) error {
+			files[filepath.Clean(path)] = &info
+			return nil
+		})
+	}
+
+	file, ok := files[filepath.Clean(fmt.Sprintf("testdata/%s", path))]
+	if !ok {
+		return ""
+	}
+
+	// Hard-coding modifiers works for the time being, but we might need a more
+	// elegant solution when we introduce new modifiers in the future.
+	switch attr {
+	case "time":
+		return (*file).ModTime().Format(time.Stamp)
+	case "time:iso":
+		return (*file).ModTime().Format(time.RFC3339)
+	case "time:year":
+		return (*file).ModTime().Format("2006")
+
+	case "size":
+		return fmt.Sprintf("%d", (*file).Size())
+	case "size:kb", "size:mb", "size:gb":
+		size := (*file).Size()
+		unit := attr[len(attr)-2:]
+		switch unit {
+		case "kb":
+			return fmt.Sprintf("%fkb", float64(size)/(1<<10))
+		case "mb":
+			return fmt.Sprintf("%fmb", float64(size)/(1<<20))
+		case "gb":
+			return fmt.Sprintf("%fgb", float64(size)/(1<<30))
+		}
+	}
+	return ""
+}
 
 // doRun executes fsql.Run and returns the output.
 func doRun(query string) string {
@@ -44,28 +89,38 @@ func TestRun_All(t *testing.T) {
 
 	cases := []Case{
 		{
-			query:    "SELECT all FROM ./testdata WHERE name = foo",
-			expected: "drwxr-xr-x\t170\tJun  4 19:24:12\tfoo\n",
+			query: "SELECT all FROM ./testdata WHERE name = foo",
+			expected: fmt.Sprintf("drwxr-xr-x\t%s\t%s\tfoo\n",
+				getAttr("foo", "size"), getAttr("foo", "time")),
 		},
 		{
-			query:    "SELECT all FROM ./testdata WHERE name LIKE qu AND size > 0",
-			expected: "drwxr-xr-x\t136\tJun  4 19:25:02\tquuz\n",
+			query: "SELECT all FROM ./testdata WHERE name LIKE qu AND size > 0",
+			expected: fmt.Sprintf("drwxr-xr-x\t%s\t%s\tquuz\n",
+				getAttr("foo/quuz", "size"), getAttr("foo/quuz", "time")),
 		},
 		{
-			query:    "SELECT all FROM ./testdata WHERE FORMAT(time, 'Jan 02 2006 15:04') > 'Jun 04 2017 23:26' AND NOT name LIKE .%",
-			expected: "drwxr-xr-x\t102\tJun  4 19:26:31\tthud\ndrwxr-xr-x\t102\tJun  4 19:26:31\tfred\n",
+			query:    "SELECT all FROM ./testdata WHERE FORMAT(time, 'Jan 02 2006 15:04') > 'Jan 01 2999 00:00'",
+			expected: "",
 		},
 		{
 			query: "SELECT all FROM ./testdata WHERE mode IS DIR",
 			expected: fmt.Sprintf("%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n",
-				"drwxr-xr-x\t204\tJun  4 19:23:29\ttestdata",
-				"drwxr-xr-x\t204\tJun  4 19:25:29\tbar",
-				"drwxr-xr-x\t102\tJun  4 19:25:24\tgarply",
-				"drwxr-xr-x\t102\tJun  4 19:25:19\txyzzy",
-				"drwxr-xr-x\t102\tJun  4 19:26:31\tthud",
-				"drwxr-xr-x\t170\tJun  4 19:24:12\tfoo",
-				"drwxr-xr-x\t136\tJun  4 19:25:02\tquuz",
-				"drwxr-xr-x\t102\tJun  4 19:26:31\tfred",
+				fmt.Sprintf("drwxr-xr-x\t%s\t%s\ttestdata",
+					getAttr(".", "size"), getAttr(".", "time")),
+				fmt.Sprintf("drwxr-xr-x\t%s\t%s\tbar",
+					getAttr("bar", "size"), getAttr("bar", "time")),
+				fmt.Sprintf("drwxr-xr-x\t%s\t%s\tgarply",
+					getAttr("bar/garply", "size"), getAttr("bar/garply", "time")),
+				fmt.Sprintf("drwxr-xr-x\t%s\t%s\txyzzy",
+					getAttr("bar/garply/xyzzy", "size"), getAttr("bar/garply/xyzzy", "time")),
+				fmt.Sprintf("drwxr-xr-x\t%s\t%s\tthud",
+					getAttr("bar/garply/xyzzy/thud", "size"), getAttr("bar/garply/xyzzy/thud", "time")),
+				fmt.Sprintf("drwxr-xr-x\t%s\t%s\tfoo",
+					getAttr("foo", "size"), getAttr("foo", "time")),
+				fmt.Sprintf("drwxr-xr-x\t%s\t%s\tquuz",
+					getAttr("foo/quuz", "size"), getAttr("foo/quuz", "time")),
+				fmt.Sprintf("drwxr-xr-x\t%s\t%s\tfred",
+					getAttr("foo/quuz/fred", "size"), getAttr("foo/quuz/fred", "time")),
 			),
 		},
 	}
@@ -87,22 +142,29 @@ func TestRun_Multiple(t *testing.T) {
 	cases := []Case{
 		{
 			query:    "SELECT name, size FROM ./testdata WHERE name = foo",
-			expected: "170\tfoo\n",
+			expected: fmt.Sprintf("%s\tfoo\n", getAttr("foo", "size")),
 		},
 		{
 			query:    "SELECT size, name FROM ./testdata WHERE name = foo",
-			expected: "170\tfoo\n",
+			expected: fmt.Sprintf("%s\tfoo\n", getAttr("foo", "size")),
 		},
 		{
 			query: "SELECT size, time, FULLPATH(name) FROM ./testdata/foo",
 			expected: fmt.Sprintf("%s\n%s\n%s\n%s\n%s\n%s\n%s\n",
-				"170\tJun  4 19:24:12\ttestdata/foo",
-				"0\tJun  4 19:22:01\ttestdata/foo/quux",
-				"136\tJun  4 19:25:02\ttestdata/foo/quuz",
-				"102\tJun  4 19:26:31\ttestdata/foo/quuz/fred",
-				"0\tJun  4 19:26:31\ttestdata/foo/quuz/fred/.gitkeep",
-				"0\tJun  4 19:24:56\ttestdata/foo/quuz/waldo",
-				"0\tJun  4 19:21:49\ttestdata/foo/qux",
+				fmt.Sprintf("%s\t%s\ttestdata/foo",
+					getAttr("foo", "size"), getAttr("foo", "time")),
+				fmt.Sprintf("%s\t%s\ttestdata/foo/quux",
+					getAttr("foo/quux", "size"), getAttr("foo/quux", "time")),
+				fmt.Sprintf("%s\t%s\ttestdata/foo/quuz",
+					getAttr("foo/quuz", "size"), getAttr("foo/quuz", "time")),
+				fmt.Sprintf("%s\t%s\ttestdata/foo/quuz/fred",
+					getAttr("foo/quuz/fred", "size"), getAttr("foo/quuz/fred", "time")),
+				fmt.Sprintf("%s\t%s\ttestdata/foo/quuz/fred/.gitkeep",
+					getAttr("foo/quuz/fred/.gitkeep", "size"), getAttr("foo/quuz/fred/.gitkeep", "time")),
+				fmt.Sprintf("%s\t%s\ttestdata/foo/quuz/waldo",
+					getAttr("foo/quuz/waldo", "size"), getAttr("foo/quuz/waldo", "time")),
+				fmt.Sprintf("%s\t%s\ttestdata/foo/qux",
+					getAttr("foo/qux", "size"), getAttr("foo/qux", "time")),
 			),
 		},
 	}
@@ -162,23 +224,26 @@ func TestRun_Size(t *testing.T) {
 	cases := []Case{
 		{
 			query:    "SELECT size FROM ./testdata WHERE name = foo",
-			expected: "170\n",
+			expected: fmt.Sprintf("%s\n", getAttr("foo", "size")),
 		},
 		{
-			query:    "SELECT FORMAT(size, KB) FROM ./testdata WHERE name = baz",
-			expected: "0.000000kb\n",
+			query:    "SELECT FORMAT(size, KB) FROM ./testdata WHERE name = foo",
+			expected: fmt.Sprintf("%s\n", getAttr("foo", "size:kb")),
 		},
 		{
-			query:    "SELECT FORMAT(size, MB) FROM ./testdata WHERE name = baz",
-			expected: "0.000000mb\n",
+			query:    "SELECT FORMAT(size, MB) FROM ./testdata WHERE name = foo",
+			expected: fmt.Sprintf("%s\n", getAttr("foo", "size:mb")),
 		},
 		{
-			query:    "SELECT FORMAT(size, GB) FROM ./testdata WHERE name = baz",
-			expected: "0.000000gb\n",
+			query:    "SELECT FORMAT(size, GB) FROM ./testdata WHERE name = foo",
+			expected: fmt.Sprintf("%s\n", getAttr("foo", "size:gb")),
 		},
 		{
-			query:    "SELECT size FROM ./testdata WHERE name LIKE qu",
-			expected: "0\n136\n0\n",
+			query: "SELECT size FROM ./testdata WHERE name LIKE qu",
+			expected: fmt.Sprintf("%s\n%s\n%s\n",
+				getAttr("foo/quux", "size"),
+				getAttr("foo/quuz", "size"),
+				getAttr("foo/qux", "size")),
 		},
 	}
 
@@ -199,19 +264,23 @@ func TestRun_Time(t *testing.T) {
 	cases := []Case{
 		{
 			query:    "SELECT time FROM ./testdata WHERE name = baz",
-			expected: "Jun  4 19:23:29\n",
+			expected: fmt.Sprintf("%s\n", getAttr("baz", "time")),
 		},
 		{
 			query:    "SELECT FORMAT(time, ISO) FROM ./testdata WHERE name = foo",
-			expected: "2017-06-04T19:24:12-04:00\n",
+			expected: fmt.Sprintf("%s\n", getAttr("foo", "time:iso")),
 		},
 		{
 			query:    "SELECT FORMAT(time, 2006) FROM ./testdata WHERE NOT name LIKE .%",
-			expected: strings.Repeat("2017\n", 14),
+			expected: strings.Repeat(fmt.Sprintf("%s\n", getAttr(".", "time:year")), 14),
 		},
 		{
-			query:    "SELECT time FROM ./testdata/foo/quuz",
-			expected: "Jun  4 19:25:02\nJun  4 19:26:31\nJun  4 19:26:31\nJun  4 19:24:56\n",
+			query: "SELECT time FROM ./testdata/foo/quuz",
+			expected: fmt.Sprintf("%s\n%s\n%s\n%s\n",
+				getAttr("foo/quuz", "time"),
+				getAttr("foo/quuz/fred", "time"),
+				getAttr("foo/quuz/fred/.gitkeep", "time"),
+				getAttr("foo/quuz/waldo", "time")),
 		},
 	}
 
