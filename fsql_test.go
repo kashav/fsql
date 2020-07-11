50fbe7536dd7c8b4ec61b503482accc3a17d561e
Kashav Madan <kshvmdn@gmail.com>
2020-07-10 23:07:03 -0400

    Don't hardcode test directory mode strings
    It's dumb to assume that these directories will have the same modes
    across fresh pulls and in automation.

--- fsql_test.go
+++ fsql_test.go
@@ -26,13 +26,13 @@ func TestRun_All(t *testing.T) {
 	cases := []Case{
 		{
 			query: "SELECT all FROM ./testdata WHERE name = foo",
-			expected: fmt.Sprintf("drwxr-xr-x\t%s\t-------\tfoo\n",
-				strings.Join(GetAttrs("foo", "size", "time"), "\t")),
+			expected: fmt.Sprintf("%s\t-------\tfoo\n",
+				strings.Join(GetAttrs("foo", "mode", "size", "time"), "\t")),
 		},
 		{
 			query: "SELECT all FROM ./testdata WHERE name LIKE waldo",
-			expected: fmt.Sprintf("-rw-r--r--\t%s\twaldo\n",
-				strings.Join(GetAttrs("foo/quuz/waldo", "size", "time", "hash"), "\t")),
+			expected: fmt.Sprintf("%s\twaldo\n",
+				strings.Join(GetAttrs("foo/quuz/waldo", "mode", "size", "time", "hash"), "\t")),
 		},
 		{
 			query:    "SELECT all FROM ./testdata WHERE FORMAT(time, 'Jan 02 2006 15:04') > 'Jan 01 2999 00:00'",
@@ -43,43 +43,43 @@ func TestRun_All(t *testing.T) {
 			expected: fmt.Sprintf(
 				strings.Repeat("%s\n", 8),
 				fmt.Sprintf(
-					"drwxr-xr-x\t%s\t-------\t%-8s",
-					strings.Join(GetAttrs(".", "size", "time"), "\t"),
+					"%s\t-------\t%-8s",
+					strings.Join(GetAttrs(".", "mode", "size", "time"), "\t"),
 					"testdata",
 				),
 				fmt.Sprintf(
-					"drwxr-xr-x\t%s\t-------\t%-8s",
-					strings.Join(GetAttrs("bar", "size", "time"), "\t"),
+					"%s\t-------\t%-8s",
+					strings.Join(GetAttrs("bar", "mode", "size", "time"), "\t"),
 					"bar",
 				),
 				fmt.Sprintf(
-					"drwxr-xr-x\t%s\t-------\t%-8s",
-					strings.Join(GetAttrs("bar/garply", "size", "time"), "\t"),
+					"%s\t-------\t%-8s",
+					strings.Join(GetAttrs("bar/garply", "mode", "size", "time"), "\t"),
 					"garply",
 				),
 				fmt.Sprintf(
-					"drwxr-xr-x\t%s\t-------\t%-8s",
-					strings.Join(GetAttrs("bar/garply/xyzzy", "size", "time"), "\t"),
+					"%s\t-------\t%-8s",
+					strings.Join(GetAttrs("bar/garply/xyzzy", "mode", "size", "time"), "\t"),
 					"xyzzy",
 				),
 				fmt.Sprintf(
-					"drwxr-xr-x\t%s\t-------\t%-8s",
-					strings.Join(GetAttrs("bar/garply/xyzzy/thud", "size", "time"), "\t"),
+					"%s\t-------\t%-8s",
+					strings.Join(GetAttrs("bar/garply/xyzzy/thud", "mode", "size", "time"), "\t"),
 					"thud",
 				),
 				fmt.Sprintf(
-					"drwxr-xr-x\t%s\t-------\t%-8s",
-					strings.Join(GetAttrs("foo", "size", "time"), "\t"),
+					"%s\t-------\t%-8s",
+					strings.Join(GetAttrs("foo", "mode", "size", "time"), "\t"),
 					"foo",
 				),
 				fmt.Sprintf(
-					"drwxr-xr-x\t%s\t-------\t%-8s",
-					strings.Join(GetAttrs("foo/quuz", "size", "time"), "\t"),
+					"%s\t-------\t%-8s",
+					strings.Join(GetAttrs("foo/quuz", "mode", "size", "time"), "\t"),
 					"quuz",
 				),
 				fmt.Sprintf(
-					"drwxr-xr-x\t%s\t-------\t%-8s",
-					strings.Join(GetAttrs("foo/quuz/fred", "size", "time"), "\t"),
+					"%s\t-------\t%-8s",
+					strings.Join(GetAttrs("foo/quuz/fred", "mode", "size", "time"), "\t"),
 					"fred",
 				),
 			),
@@ -89,7 +89,7 @@ func TestRun_All(t *testing.T) {
 	for _, c := range cases {
 		actual := DoRun(c.query)
 		if !reflect.DeepEqual(c.expected, actual) {
-			t.Fatalf("\nExpected:\n%v\nGot:\n%v", c.expected, actual)
+			t.Fatalf("%s\nExpected:\n%v\nGot:\n%v", c.query, c.expected, actual)
 		}
 	}
 }
@@ -155,7 +155,7 @@ func TestRun_Multiple(t *testing.T) {
 	for _, c := range cases {
 		actual := DoRun(c.query)
 		if !reflect.DeepEqual(c.expected, actual) {
-			t.Fatalf("\nExpected:\n%v\nGot:\n%v", c.expected, actual)
+			t.Fatalf("%s\nExpected:\n%v\nGot:\n%v", c.query, c.expected, actual)
 		}
 	}
 }
@@ -194,7 +194,7 @@ func TestRun_Name(t *testing.T) {
 	for _, c := range cases {
 		actual := DoRun(c.query)
 		if !reflect.DeepEqual(c.expected, actual) {
-			t.Fatalf("\nExpected:\n%v\nGot:\n%v", c.expected, actual)
+			t.Fatalf("%s\nExpected:\n%v\nGot:\n%v", c.query, c.expected, actual)
 		}
 	}
 }
@@ -236,7 +236,7 @@ func TestRun_Size(t *testing.T) {
 	for _, c := range cases {
 		actual := DoRun(c.query)
 		if !reflect.DeepEqual(c.expected, actual) {
-			t.Fatalf("\nExpected:\n%v\nGot:\n%v", c.expected, actual)
+			t.Fatalf("%s\nExpected:\n%v\nGot:\n%v", c.query, c.expected, actual)
 		}
 	}
 }
@@ -275,7 +275,7 @@ func TestRun_Time(t *testing.T) {
 	for _, c := range cases {
 		actual := DoRun(c.query)
 		if !reflect.DeepEqual(c.expected, actual) {
-			t.Fatalf("\nExpected:\n%v\nGot:\n%v", c.expected, actual)
+			t.Fatalf("%s\nExpected:\n%v\nGot:\n%v", c.query, c.expected, actual)
 		}
 	}
 }
@@ -289,22 +289,31 @@ func TestRun_Mode(t *testing.T) {
 	cases := []Case{
 		{
 			query:    "SELECT mode FROM ./testdata WHERE name = foo",
-			expected: "drwxr-xr-x\n",
+			expected: fmt.Sprintf("%s\n", GetAttrs("foo", "mode")[0]),
 		},
 		{
 			query:    "SELECT mode FROM ./testdata WHERE name = baz",
-			expected: "-rwxr-xr-x\n",
+			expected: fmt.Sprintf("%s\n", GetAttrs("baz", "mode")[0]),
 		},
 		{
-			query:    "SELECT mode FROM ./testdata WHERE mode IS DIR",
-			expected: strings.Repeat("drwxr-xr-x\n", 8),
+			query: "SELECT mode FROM ./testdata WHERE mode IS DIR",
+			expected: fmt.Sprintf(strings.Repeat("%s\n", 8),
+				GetAttrs("", "mode")[0],
+				GetAttrs("foo", "mode")[0],
+				GetAttrs("foo/quuz", "mode")[0],
+				GetAttrs("foo/quuz/fred", "mode")[0],
+				GetAttrs("bar", "mode")[0],
+				GetAttrs("bar/garply", "mode")[0],
+				GetAttrs("bar/garply/xyzzy", "mode")[0],
+				GetAttrs("bar/garply/xyzzy/thud", "mode")[0],
+			),
 		},
 	}
 
 	for _, c := range cases {
 		actual := DoRun(c.query)
 		if !reflect.DeepEqual(c.expected, actual) {
-			t.Fatalf("\nExpected:\n%v\nGot:\n%v", c.expected, actual)
+			t.Fatalf("%s\nExpected:\n\"%v\"\nGot:\n\"%v\"", c.query, c.expected, actual)
 		}
 	}
 }
@@ -321,7 +330,7 @@ func TestRun_Hash(t *testing.T) {
 	for _, c := range cases {
 		actual := DoRun(c.query)
 		if !reflect.DeepEqual(c.expected, actual) {
-			t.Fatalf("\nExpected:\n%v\nGot:\n%v", c.expected, actual)
+			t.Fatalf("%s\nExpected:\n%v\nGot:\n%v", c.query, c.expected, actual)
 		}
 	}
 }
@@ -361,6 +370,8 @@ func GetAttrs(path string, attrs ...string) []string {
 				return []string{}
 			}
 			result[i] = hex.EncodeToString(h.Sum(nil))[:7]
+		case "mode":
+			result[i] = (*file).Mode().String()
 		case "size":
 			result[i] = fmt.Sprintf("%d", (*file).Size())
 		case "size:kb", "size:mb", "size:gb":
